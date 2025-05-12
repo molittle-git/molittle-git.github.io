4ae5f0f981dcd2225bd9013c02eea550493d7450
molittle-git <3466954759@qq.com>
2025-05-12 11:12:53 +0800

    更新: YYYY-05-2025年5月12日 11:12:52

--- js/fuction.js
+++ js/fuction.js
@@ -5,17 +5,26 @@ function share() {
     btf.snackbarShow("本页链接已复制到剪切板，快去分享吧~")
 }
 window.onscroll = percent;// 执行函数
+
 // 页面百分比
 function percent() {
-    let a = document.documentElement.scrollTop || window.pageYOffset, // 卷去高度
-        b = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight, document.body.offsetHeight, document.documentElement.offsetHeight, document.body.clientHeight, document.documentElement.clientHeight) - document.documentElement.clientHeight, // 整个网页高度
-        result = Math.round(a / b * 100), // 计算百分比
-        up = document.querySelector("#go-up") // 获取按钮
+    // 检查页面中是否存在 card-toc 元素
+    const cardToc = document.getElementById('card-toc');
+    if (!cardToc) {
+        return; // 如果不存在，直接返回，不执行后续代码
+    }
 
-    if (result <= 95) {
+    // 获取百分比文本内容
+    const percentage = cardToc.querySelector('.toc-percentage').textContent.trim();
+
+    // 获取 go-up 元素
+    let up = document.querySelector("#go-up");
+
+    // 根据百分比设置样式和内容
+    if (percentage <= 99) {
         up.childNodes[0].style.display = 'none'
         up.childNodes[1].style.display = 'block'
-        up.childNodes[1].childNodes[0].innerHTML = result;
+        up.childNodes[1].innerHTML = percentage;
     } else {
         up.childNodes[1].style.display = 'none'
         up.childNodes[0].style.display = 'block'
